e607663e2ed074dec841726db16772e58951814c
Mo <eellrraawwii@gmail.com>
2025-09-13 20:20:34 +0300

    fix regions model

--- lib/src/features/regions/data/models/region.dart
+++ lib/src/features/regions/data/models/region.dart
@@ -1,6 +1,4 @@
 import 'package:freezed_annotation/freezed_annotation.dart';
-import 'package:medusa_admin_dart_client/src/features/fulfillment_providers/data/models/fulfillment_provider.dart';
-import 'package:medusa_admin_dart_client/src/features/payments/data/models/payment_provider.dart';
 import 'package:medusa_admin_dart_client/src/core/models/country.dart';
 
 part 'region.freezed.dart';
